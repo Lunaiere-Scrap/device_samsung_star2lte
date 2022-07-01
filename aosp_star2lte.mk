d7c7ed4295e6ab825a8e7241be279529273f444a
Seraphine <103412573+Seraphine3@users.noreply.github.com>
2022-07-01 21:04:46 +0800

    starlte: adapt to star2lte

--- aosp_star2lte.mk
+++ aosp_star2lte.mk
@@ -27,11 +27,11 @@ $(call inherit-product, device/samsung/star2lte/device.mk)
 TARGET_SCREEN_HEIGHT := 2960
 TARGET_SCREEN_WIDTH := 1440
 
-## Inherit some common Pixel OS stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+## Inherit some common awaken stuff
+$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
 
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := aosp_star2lte
+PRODUCT_NAME := awaken_star2lte
 PRODUCT_DEVICE := star2lte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G965F
