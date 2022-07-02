e9c6ddcc4403a7741265c8284603ff30217b0b6b
Seraphine <103412573+Seraphine3@users.noreply.github.com>
2022-07-02 07:26:56 -0400

    starlte: Adapt > Corvus

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_star2lte.mk
+    $(LOCAL_DIR)/corvus_star2lte.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_star2lte-eng \
-    aosp_star2lte-user \
-    aosp_star2lte-userdebug
+    corvus_star2lte-eng \
+    corvus_star2lte-user \
+    corvus_star2lte-userdebug
