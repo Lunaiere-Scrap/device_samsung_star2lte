298c2d5589145cf192fce7856250d74b01ca9a34
Seraphine <103412573+Seraphine3@users.noreply.github.com>
2022-07-13 16:50:53 +0800

    starlte: ricedroid bringup

--- aosp_star2lte.mk
+++ aosp_star2lte.mk
@@ -24,14 +24,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 $(call inherit-product, device/samsung/star2lte/device.mk)
 
 # Boot Animation
-TARGET_SCREEN_HEIGHT := 2960
-TARGET_SCREEN_WIDTH := 1440
+TARGET_BOOT_ANIMATION_RES := 1440
 
-## Inherit some common Pixel OS stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# gapps build flag, if not defined build type is vanilla
+# GAPPS package is similar to core gapps
+WITH_GAPPS := true
+
+# maintainer flag
+RICE_MAINTAINER := Mia
+
+#Face unlock 
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+## Inherit some common ricedroid stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := aosp_star2lte
+PRODUCT_NAME := lineage_star2lte
 PRODUCT_DEVICE := star2lte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G965F
